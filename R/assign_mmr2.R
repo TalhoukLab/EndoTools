ae0d20d3a144ef3d7c066edb5a3bef40b9ca68ee
Derek <dchiu@bccrc.ca>
2021-08-18 16:47:32 -0700

    add assign_mmr2 and assign_mmr4

---
+++ R/assign_mmr2.R
@@ -0,0 +1,21 @@
+#' Assign MMR status by 2 IHC markers
+#'
+#' Assign MMR status by 2 IHC markers, MSH6 and PMS2.
+#'
+#' MMR status, intact or deficient, is determined by IHC markers MSH6 and PMS2
+#' consolidated from TMA scoring. If both markers are intact, then MMR status is
+#' intact. Otherwise, MMR status is deficient (including subclonal and unknown
+#' markers).
+#'
+#' @param msh6 MSH6 marker
+#' @param pms2 PMS2 marker
+#' @return MMR status, either "intact" or "deficient"
+#' @author Samuel Leung, Derek Chiu
+#' @export
+assign_mmr2 <- function(msh6, pms2) {
+  if (msh6 == "intact" & pms2 == "intact") {
+    return("intact")
+  } else {
+    return("deficient")
+  }
+}
---
+++ R/assign_mmr4.R
@@ -0,0 +1,22 @@
+#' Assign MMR status by 4 IHC markers
+#'
+#' Assign MMR status by 4 IHC markers, MSH6, PMS2, MLH1, and MSH2.
+#'
+#' MMR status, intact or deficient, is determined by IHC markers MSH6, PMS2,
+#' MLH1, and MSH2 consolidated from TMA scoring. If all markers are intact, then
+#' MMR status is intact. Otherwise, MMR status is deficient (including subclonal
+#' and unknown markers).
+#'
+#' @inheritParams assign_mmr2
+#' @param mlh1 MLH1 marker
+#' @param msh2 MSH2 marker
+#' @return MMR status, either "intact" or "deficient"
+#' @author Samuel Leung, Derek Chiu
+#' @export
+assign_mmr4 <- function(msh6, pms2, mlh1, msh2) {
+  if (msh6 == "intact" & pms2 == "intact" & mlh1 == "intact" & msh2 == "intact") {
+    return("intact")
+  } else {
+    return("deficient")
+  }
+}
