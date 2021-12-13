f5e78be209adf41ee2e4ef92ce07de91337115fb
Derek Chiu <dchiu@bccrc.ca>
2021-12-13 11:49:57 -0800

    add unit tests to complete coverage

--- tests/testthat/test-assign_ace.R
+++ tests/testthat/test-assign_ace.R
@@ -32,3 +32,40 @@ test_that("ace27 works", {
   expect_equal(sum(emdb$ace27 == ">=Moderate", na.rm = TRUE), 177)
   expect_equal(sum(emdb$ace27 == "Severe", na.rm = TRUE), 71)
 })
+
+test_that("NOS can be separated", {
+  emdb$ace27 <- assign_ace27(
+    ace_mi = emdb$ace_mi,
+    ace_chf = emdb$ace_chf,
+    ace_cad = emdb$ace_cad,
+    ace_arr = emdb$ace_arr,
+    ace_htn = emdb$ace_htn,
+    ace_vd = emdb$ace_vd,
+    ace_pad = emdb$ace_pad,
+    ace_res = emdb$ace_res,
+    ace_hep = emdb$ace_hep,
+    ace_sto = emdb$ace_sto,
+    ace_pan = emdb$ace_pan,
+    ace_rd = emdb$ace_rd,
+    ace_dm = emdb$ace_dm,
+    ace_str = emdb$ace_str,
+    ace_dem = emdb$ace_dem,
+    ace_par = emdb$ace_par,
+    ace_neu = emdb$ace_neu,
+    ace_psy = emdb$ace_psy,
+    ace_rhe = emdb$ace_rhe,
+    ace_aid = emdb$ace_aid,
+    ace_st = emdb$ace_st,
+    ace_lm = emdb$ace_lm,
+    ace_lym = emdb$ace_lym,
+    ace_alc = emdb$ace_alc,
+    ace_id = emdb$ace_id,
+    ace_obe = emdb$ace_obe,
+    separate_nos = TRUE
+  )
+  expect_equal(sum(emdb$ace27 == "None", na.rm = TRUE), 111)
+  expect_equal(sum(emdb$ace27 == ">=Mild", na.rm = TRUE), 331)
+  expect_equal(sum(emdb$ace27 == ">=Moderate", na.rm = TRUE), 177)
+  expect_equal(sum(emdb$ace27 == "Severe", na.rm = TRUE), 71)
+  expect_equal(sum(emdb$ace27 == "NOS", na.rm = TRUE), 110)
+})
--- tests/testthat/test-assign_esmo.R
+++ tests/testthat/test-assign_esmo.R
@@ -27,6 +27,23 @@ test_that("esmo2016 works", {
   expect_equal(sum(emdb$esmo2016 == "metastatic", na.rm = TRUE), 6)
 })
 
+test_that("esmo2016 with residual works", {
+  emdb$esmo2016 <- assign_esmo2016(
+    stage = emdb$stage_full,
+    grade = emdb$grade_rev,
+    hist_gr = emdb$hist_rev_gr,
+    myo = emdb$myo,
+    lvi = emdb$lvi,
+    residual = emdb$residual
+  )
+  expect_equal(sum(emdb$esmo2016 == "low", na.rm = TRUE), 36)
+  expect_equal(sum(emdb$esmo2016 == "intermediate", na.rm = TRUE), 21)
+  expect_equal(sum(emdb$esmo2016 == "high-intermediate", na.rm = TRUE), 99)
+  expect_equal(sum(emdb$esmo2016 == "high", na.rm = TRUE), 398)
+  expect_equal(sum(emdb$esmo2016 == "advanced", na.rm = TRUE), 35)
+  expect_equal(sum(emdb$esmo2016 == "metastatic", na.rm = TRUE), 6)
+})
+
 test_that("esmo2020 works", {
   emdb$esmo2020 <- assign_esmo2020(
     stage = emdb$stage_full,
